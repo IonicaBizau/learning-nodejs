3a3ba9540414a2318320bf6887e7279f5240a7e4
Ionică Bizău <bizauionica@yahoo.com>
2013-12-19 18:44:19 +0200

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+var npm = require("npm");
+npm.load({
+    loaded: false
+}, function (err) {
+  // catch errors
+  npm.commands.install(createNpmDependenciesArray('./tests/package'), function (er, data) {
+    // log the error or data
+  });
+  npm.on("log", function (message) {
+    // log the progress of the installation
+    console.log(message);
+  });
+});
+
+function createNpmDependenciesArray (packageFilePath) {
+    var p = require(packageFilePath);
+    if (!p.dependencies) return [];
+    
+    var deps = [];
+    for (var mod in p.dependencies) {
+        deps.push(mod + "@" + p.dependencies[mod]);
+    }
+
+    return deps;
+}
