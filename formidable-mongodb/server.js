abae72c3a4580aaeba3418101d7c8780896e760c
Ionică Bizău <bizauionica@yahoo.com>
2014-06-16 09:40:05 +0300

    Removed end handler

--- formidable-mongodb/server.js
+++ formidable-mongodb/server.js
@@ -13,30 +13,28 @@ var file = new(static.Server)('./public');
 // Create server
 var srv = http.createServer(function(req, res){
     if(req.method === "GET"){
-        req.on('end', function(){
-            file.serve(req, res);
-        });
+        file.serve(req, res);
     }
-    else{       
+    else{
         var form = new formidable.IncomingForm();
         var fieldsInformation = null;
         form.parse(req, function(err, fields) {
             fieldsInformation = fields;
-        });   
-        
+        });
+
         db.open(function(err, db) {
             if (err) return res.end("A problem ocured. Try again later.");
-                    
+
             db.collection("col_one", function(err, myCol) {
                 if (err) return res.end("A problem ocured. Try again later.");
-                
+
                 var objectToInsert = {
                     "firstName" : fieldsInformation.firstName,
                     "lastName" : fieldsInformation.lastName,
                     "age" : fieldsInformation.age,
                     "gender" : fieldsInformation.gender,
                 };
-                
+
                 myCol.insert(objectToInsert, function(){
                     res.end("Successfully saved.");
                     db.close();
