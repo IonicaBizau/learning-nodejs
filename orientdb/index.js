c7055c94fa37495a1b8266b98be627eaa96d7502
Ionică Bizău <bizauionica@yahoo.com>
2014-09-30 20:22:17 +0300

    Use guest user instead of root

--- orientdb/index.js
+++ orientdb/index.js
@@ -1,9 +1,5 @@
 var OrientDB = require("oriento");
-var server = OrientDB({
-    // TODO Do not use root credentials
-    username: "root",
-    password: "3B2BE02AF63AAD88891E7E5501946D08CA10393B516D1062C23B8D110BD3CFD0"
-});
+var server = OrientDB({ username: "guest", password: "guest" });
 
 var db = server.use({
     name: "countries",
