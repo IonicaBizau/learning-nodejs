16fe50e567b93b534ed649f69396ccf77e3d9721
Ionică Bizău <bizauionica@gmail.com>
2015-11-19 10:52:01 +0200

    Added the click handler

--- jasmine-test/js/main.js
+++ jasmine-test/js/main.js
@@ -2,4 +2,15 @@ setTimeout(function () {
     document.querySelector("#container").innerHTML = "Hello Jasmine";
 }, 1000);
 
-document.title = "Testing Jasmine1";
+document.title = "Testing Jasmine";
+
+window.onload = function () {
+
+    var btnEl = document.querySelector(".click-me")
+      , resEl = document.querySelector(".result")
+      ;
+
+    btnEl.addEventListener("click", function () {
+        resEl.innerHTML = "You clicked!";
+    });
+};
